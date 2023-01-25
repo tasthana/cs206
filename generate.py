93a09b1c5dd38156f811d7526cca9745dec3b421
tasthana <tasthana@outlook.com>
2023-01-24 20:13:08 -0500

    Changed length, width, and height and starts at ground level.

--- generate.py
+++ generate.py
@@ -1,2 +1,18 @@
 
-pass
+import pyrosim.pyrosim as pyrosim
+
+pyrosim.Start_SDF("box.sdf")
+
+length = 1
+width = 2
+height = 3
+
+x = 0
+y = 0
+z = 1.5
+
+pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length, width, height])
+
+pyrosim.End()
+
+
