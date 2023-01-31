5bd5452337083fa8d4d7cc0fc44c550477aa0c4a
tasthana <tasthana@outlook.com>
2023-01-30 19:42:02 -0500

    25 towers running code

--- simulate.py
+++ simulate.py
@@ -8,9 +8,11 @@ physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("box.sdf")
+robotId = p.loadURDF("body.urdf")
 
-for x in range(0, 1001):
+p.loadSDF("boxes.sdf")
+
+for x in range(0, 2001):
     time.sleep(.001)
     p.stepSimulation()
     print(x)
