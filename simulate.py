706b85bb0c4284b53cd98e620af6272707fc7c66
tasthana <tasthana@outlook.com>
2023-02-06 17:57:48 -0500

    Steps 1-14 for Assignment 3

--- simulate.py
+++ simulate.py
@@ -3,17 +3,21 @@
 import pybullet as p
 import pybullet_data
 import time 
+import pyrosim.pyrosim as pyrosim
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
+
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
+pyrosim.Prepare_To_Simulate(robotId)
 for x in range(0, 2001):
     time.sleep(.001)
     p.stepSimulation()
-    print(x)
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    print(backLegTouch)
 p.disconnect()
 
