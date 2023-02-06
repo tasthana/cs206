690ce8fe095ceb32423d21a9b8cc9924bb506f86
tasthana <tasthana@outlook.com>
2023-02-06 18:15:52 -0500

    krisna code

--- simulate.py
+++ simulate.py
@@ -8,13 +8,12 @@ physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-robotId = p.loadURDF("body.urdf")
+#robotId = p.loadURDF("body.urdf")
 
 p.loadSDF("boxes.sdf")
 
 for x in range(0, 2001):
     time.sleep(.001)
-    p.stepSimulation()
     print(x)
 p.disconnect()
 
