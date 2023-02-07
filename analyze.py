bf726a85728c62f22ce65a632033cc04c5156fdb
tasthana <tasthana@outlook.com>
2023-02-06 23:26:01 -0500

    all files for assignment 3

--- analyze.py
+++ analyze.py
@@ -3,8 +3,23 @@
 
 
 import numpy
+import matplotlib.pyplot as plot
+import os 
+
+# load 
+backLegSensorValues = numpy.load(os.path.join("data", "BackLegSensorValues.npy"))
+frontLegSensorValues = numpy.load(os.path.join("data", "FrontLegSensorValues.npy"))
+
+# print values 
+print(backLegSensorValues)
+print(frontLegSensorValues)
+
+# plotting 
+plot.plot(backLegSensorValues, label='Back Leg', linewidth=3.5)
+plot.plot(frontLegSensorValues,  label='Front Leg')
 
 
+plot.legend()
+plot.show()
 
-numpy.load("backLegSensorValues"
 
--- simulate.py
+++ simulate.py
@@ -4,6 +4,8 @@ import pybullet as p
 import pybullet_data
 import time 
 import pyrosim.pyrosim as pyrosim
+import numpy
+import os 
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -14,10 +16,18 @@ robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
-for x in range(0, 2001):
+
+backLegSensorValues = numpy.zeros(100)
+frontLegSensorValues = numpy.zeros(100)
+for x in range(0, 100):
     time.sleep(.001)
     p.stepSimulation()
-    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    print(backLegTouch)
+    backLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    print(backLegSensorValues)
+    print(frontLegSensorValues)
+numpy.save(os.path.join("data", "BackLegSensorValues.npy"), backLegSensorValues)
+numpy.save(os.path.join("data", "FrontLegSensorValues.npy"), frontLegSensorValues)
 p.disconnect()
+
 
