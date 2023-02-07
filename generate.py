b53d1982f8d6755669e3d0b23ce99cabd07a24bd
Tushar Asthana <101343079+tasthana@users.noreply.github.com>
2023-02-06 21:14:43 -0500

    working three links and two joints

--- generate.py
+++ generate.py
@@ -1,43 +1,23 @@
-
 import pyrosim.pyrosim as pyrosim
-
-
-# declare variables 
-
-length = 1
-width = 1
-height = 1
-
-x = 0
-y = 0
-z = .5
-
-
-# define functions
-
+ 
 def Create_World():
-  pyrosim.Start_SDF("world.sdf")
-  pyrosim.Send_Cube(name="Box", pos=[4,4,z] , size=[length, width, height])
-  pyrosim.End()
-
-def Create_Robot():
-  pyrosim.Start_URDF("body.urdf")
-  pyrosim.Send_Cube(name="Torso", pos=[0,0,2] , size=[length, width, height])
-
-  pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [1,0,.5])
-  pyrosim.Send_Cube(name="BackLeg", pos=[0,0,.5] , size=[length, width, height])
-
-  pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [-1,0,.5])
-  pyrosim.Send_Cube(name="FrontLeg", pos=[0,0,.5] , size=[length, width, height])
-
-  pyrosim.End()
-
-# Call Functions
-
+	pyrosim.Start_SDF("world.sdf")
+	pyrosim.Send_Cube(name="Box", pos=[2,2,0.5] , size=[1,1,1])
+	pyrosim.End()
+ 
+def Create_Robot(torso_coordinates, link_size):
+	pyrosim.Start_URDF("body.urdf")
+	joint_torso_backleg_coordinates = [torso_coordinates[0]-0.5, 0, torso_coordinates[2]-(0.5*link_size[2])]
+	backLeg_coordinates = [link_size[0]*(-0.5),0,link_size[2]*(-0.5)]
+	joint_torso_frontleg_coordinates = [torso_coordinates[0]+0.5, 0, torso_coordinates[2]-(0.5*link_size[2])]
+	frontLeg_coordinates = [link_size[0]*0.5,0,link_size[2]*(-0.5)]
+	pyrosim.Send_Cube(name="Torso", pos=torso_coordinates , size=link_size) # absolute
+	pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = joint_torso_backleg_coordinates)
+	pyrosim.Send_Cube(name="BackLeg", pos=backLeg_coordinates , size=link_size)
+	pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = joint_torso_frontleg_coordinates)
+	pyrosim.Send_Cube(name="FrontLeg", pos=frontLeg_coordinates , size=link_size)
+	pyrosim.End()
+link_size = [1, 1, 1]
+torso_coordinates = [1, 0, 1.5*link_size[2]]
 Create_World()
-Create_Robot()
-
-
-
-
-
+Create_Robot(torso_coordinates, link_size)
